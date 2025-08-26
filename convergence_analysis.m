3ed721d353ae1d4c0ab33359c1d1ab65059c39e9
msvdsudarsan <msvdsudarsan@gmail.com>
2025-08-26 20:11:37 +0530

    Create convergence_analysis.m

---
+++ convergence_analysis.m
@@ -0,0 +1,7 @@
+% convergence_analysis.m
+% Placeholder script to run convergence tests and produce plots.
+
+% Example usage:
+%   example1_small_system(); % runs a built-in convergence test
+
+error('convergence_analysis is a placeholder. Use example1_small_system or create a custom script.');
