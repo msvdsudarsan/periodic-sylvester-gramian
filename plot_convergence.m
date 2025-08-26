fb9260df4d1a621ad2af8937db729f8e2b0f6d9f
msvdsudarsan <msvdsudarsan@gmail.com>
2025-08-26 20:12:35 +0530

    Create plot_convergence.m

---
+++ plot_convergence.m
@@ -0,0 +1,4 @@
+% plot_convergence.m
+% Placeholder: implement plotting routines to visualize quadrature convergence.
+
+error('plot_convergence is a placeholder. Add plotting code to visualize results.');
