41298a6eb5590d8185f11478f9f010b440252dee
msvdsudarsan <msvdsudarsan@gmail.com>
2025-08-26 20:09:14 +0530

    Create block_sylvester_propagate.m

---
+++ block_sylvester_propagate.m
@@ -0,0 +1,6 @@
+% block_sylvester_propagate.m
+% Placeholder: implement block-wise propagation algorithm from manuscript here.
+
+function out = block_sylvester_propagate(varargin)
+    error('block_sylvester_propagate is a placeholder. Please implement the algorithm from the paper.');
+end
