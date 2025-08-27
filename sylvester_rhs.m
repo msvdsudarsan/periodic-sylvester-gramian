dc74e277bed70dd03568fc81dcc0e076297d1433
msvdsudarsan <msvdsudarsan@gmail.com>
2025-08-28 02:17:45 +0530

    Delete sylvester_rhs.m

--- sylvester_rhs.m
+++
@@ -1,7 +0,0 @@
-function dZ = sylvester_rhs(t, Z_vec, A_func, B_func)
-% Vectorized RHS: dZ/dt = A(t) Z + Z B(t)
-n = round(sqrt(numel(Z_vec)));
-Z = reshape(Z_vec, n, n);
-dZ_mat = A_func(t) * Z + Z * B_func(t);
-dZ = dZ_mat(:);
-end
