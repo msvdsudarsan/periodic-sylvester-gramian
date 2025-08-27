dd7214d7912c597b8575be03c58588d43a054b47
msvdsudarsan <msvdsudarsan@gmail.com>
2025-08-27 18:24:28 +0530

    sylvester_rhs.m

--- sylvester_rhs.m
+++ sylvester_rhs.m
@@ -1,8 +1,7 @@
-% sylvester_rhs.m
-% Placeholder for a separate RHS file if you want to use it outside compute_periodic_gramian.
-
-function dz_dt = sylvester_rhs(t, Z, A_func, B_func)
-    % If Z is matrix form:
-    dZ_dt = A_func(t) * Z + Z * B_func(t);
-    dz_dt = dZ_dt;
+function dZ = sylvester_rhs(t, Z_vec, A_func, B_func)
+% Vectorized RHS: dZ/dt = A(t) Z + Z B(t)
+n = round(sqrt(numel(Z_vec)));
+Z = reshape(Z_vec, n, n);
+dZ_mat = A_func(t) * Z + Z * B_func(t);
+dZ = dZ_mat(:);
 end
