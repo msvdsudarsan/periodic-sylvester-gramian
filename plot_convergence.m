fec641784cc251bc6af7ab967f4511be2e6af9aa
msvdsudarsan <msvdsudarsan@gmail.com>
2025-08-28 02:17:06 +0530

    Delete plot_convergence.m

--- plot_convergence.m
+++
@@ -1,6 +0,0 @@
-function plot_convergence(Ns, sigmas)
-figure; 
-semilogy(Ns, abs(sigmas - sigmas(end) + eps), 'o-','LineWidth',1.2);
-grid on; xlabel('Quadrature nodes N'); ylabel('|sigma_{min}^{(N)} - sigma_{min}^{(N_{max})}|');
-title('Convergence of \sigma_{min}(W) with quadrature refinement');
-end
