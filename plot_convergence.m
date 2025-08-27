deccf32c1dd3e3a0d47b67edefea8c697d7a1041
msvdsudarsan <msvdsudarsan@gmail.com>
2025-08-27 18:32:21 +0530

    plot_convergence.m

--- plot_convergence.m
+++ plot_convergence.m
@@ -1,4 +1,6 @@
-% plot_convergence.m
-% Placeholder: implement plotting routines to visualize quadrature convergence.
-
-error('plot_convergence is a placeholder. Add plotting code to visualize results.');
+function plot_convergence(Ns, sigmas)
+figure; 
+semilogy(Ns, abs(sigmas - sigmas(end) + eps), 'o-','LineWidth',1.2);
+grid on; xlabel('Quadrature nodes N'); ylabel('|sigma_{min}^{(N)} - sigma_{min}^{(N_{max})}|');
+title('Convergence of \sigma_{min}(W) with quadrature refinement');
+end
